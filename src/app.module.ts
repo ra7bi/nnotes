712475e252e666dcaeb9aa8dd928effd90a9da47
ra7bi <ra7bi@hotmail.com>
2024-08-11 01:51:28 +0400

    Load entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Note } from './notes/note.entity';
+import { Category } from './categories/category.entity';
+import { UserController } from './users/users.controller';
+import { CategoryController } from './categories/categories.controller';
+import { NoteController } from './notes/notes.controller';
+import { UserService } from './users/users.service';
+import { CategoryService } from './categories/categories.service';
+import { NoteService } from './notes/notes.service';
 
 @Module({
   imports: [
@@ -10,9 +19,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',   // Replace with your MySQL username
       password: '123123', // Replace with your MySQL password
       database: 'notes',   // Replace with your MySQL database name
-      entities: [],       // Specify your entities here
+      entities: [User,Note,Category],       // Specify your entities here
       synchronize: false,  // Set to false in production
     }),
+    TypeOrmModule.forFeature([User, Note, Category]),
   ],
+  controllers: [UserController,CategoryController,NoteController],
+  providers: [UserService,CategoryService,NoteService],
 })
 export class AppModule {}
